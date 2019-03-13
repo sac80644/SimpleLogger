43dfd4beb5eb943dd9223f9eb879bb8dcc230579
sac80644 <jason@gallowaytech.com>
2019-03-13 18:49:17 -0500

    Add project files.

---
+++ SimpleLogger/ILogger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SimpleLogger
+{
+    public interface ILogger
+    {
+        bool IsDebugEnabled { get; }
+        bool IsInfoEnabled { get; }
+        bool IsWarnEnabled { get; }
+        bool IsErrorEnabled { get; }
+        bool IsFatalEnabled { get; }
+
+        void Debug(object message);
+        void Debug(object message, Exception exception);
+        void Error(object message);
+        void Error(object message, Exception exception);
+        void Fatal(object message);
+        void Fatal(object message, Exception exception);
+        void Info(object message);
+        void Info(object message, Exception exception);
+        void Warn(object message);
+        void Warn(object message, Exception exception);
+    }
+}
---
+++ SimpleLogger/Logger.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using log4net;
+using log4net.Core;
+
+namespace SimpleLogger
+{
+    public class Logger : ILogger
+    {
+        public bool IsDebugEnabled { get; }
+
+        public bool IsInfoEnabled { get; }
+
+        public bool IsWarnEnabled { get; }
+
+        public bool IsErrorEnabled { get; }
+
+        public bool IsFatalEnabled { get; }
+
+        private ILog _log;
+
+        public Logger()
+        {
+            _log = LogManager.GetLogger("DefaultLog");
+        }
+
+        public ILog GetLogger(string name)
+        {
+            return LogManager.GetLogger(name);
+        }
+
+        public void Debug(object message)
+        {
+            _log.Debug(message);
+        }
+
+        public void Debug(object message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(object message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Error(object message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fatal(object message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Fatal(object message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Info(object message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Info(object message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warn(object message)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Warn(object message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
